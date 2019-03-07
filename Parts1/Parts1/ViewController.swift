72a9cbdf1a9fdad2bdff346307e2aae93d0f46d3
ihara moeka <ihara.moeka@gmail.com>
2019-03-07 16:13:27 +0900

    first commit

---
+++ Parts1/Parts1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parts1
+//
+//  Created by 伊原萌夏 on 2019/03/07.
+//  Copyright © 2019年 伊原萌夏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
