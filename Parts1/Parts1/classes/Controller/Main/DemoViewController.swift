fb9d72ee3d39a7572f1ae2cae6de93dd78cbbc5b
ihara moeka <ihara.moeka@gmail.com>
2019-03-07 17:58:33 +0900

    add checkbox button

---
+++ Parts1/Parts1/classes/Controller/Main/DemoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DemoViewController.swift
+//  Parts1
+//
+//  Created by 伊原萌夏 on 2019/03/07.
+//  Copyright © 2019年 伊原萌夏. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Property
+class DemoViewController: BaseViewController{
+    
+}
+
+// MARK: - Life cycle
+extension DemoViewController {
+    override func loadView() {
+        super.loadView()
+    }
+}
+
+// MARK: - Protocol
+extension DemoViewController {
+    
+}
+
+// MARK: - method
+extension DemoViewController {
+    
+}
